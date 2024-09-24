6a925b70ed6ab6e24d17bc4f903b665211146aab
Hieu B2111841 <hieub2111841@student.ctu.edu.vn>
2024-09-24 21:43:36 +0700

    Add Manager Scripts and Set ProjectSettings

---
+++ Assets/Code/Scripts/_Managers/Extensions/ComponentExtension.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace Managers.Extension
+{
+    public static class ComponentExtension
+    {
+        /// <summary>
+        ///     Try to get the component in this gameObject, in its children and parent components
+        /// </summary>
+        /// <returns>
+        ///     Returns false if <paramref name="component"/> is still null after loading
+        /// </returns>
+        public static bool LoadComponent<T>(this Component self, ref T component, bool isDebug = false) where T : Component
+        {
+            if (component != null) return true;
+
+            component = self.GetComponent<T>();
+            component ??= self.GetComponentInChildren<T>();
+            component ??= self.GetComponentInParent<T>();
+
+            if (isDebug)
+                if (component) Debug.Log($"{self.name}: {component.name} load successed!", self);
+                else Debug.LogWarning($"{self.name}: {typeof(T)} load failed!", self);
+
+            return component != null;
+        }
+
+
+        public static bool CheckComponent<T>(this Component self, T component, bool isDebug = false) where T : Component
+        {
+            if(isDebug)
+                if (component == null) Debug.LogWarning($"{self.name}: {typeof(T)} is null!", self);
+
+            return component != null;
+        }
+    }
+}
---
+++ Assets/Code/Scripts/_Managers/Inputs/MouseButton.cs
@@ -0,0 +1,10 @@
+﻿namespace Managers
+{
+    public enum MouseButton : int
+    {
+        None = -1,
+        Left = 0,
+        Right = 1,
+        Middle = 2,
+    }
+}
